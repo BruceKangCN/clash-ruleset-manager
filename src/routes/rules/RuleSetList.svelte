586c3b7de2de49ca7a4194f18d0e91fb9a2aed00
Bruce Kang <brucekangcn@gmail.com>
2025-04-07 14:23:40 +0800

    use modal to block user input on sorting

--- src/routes/rules/RuleSetList.svelte
+++ src/routes/rules/RuleSetList.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import { Modal } from "flowbite-svelte";
     import { dndzone, type DndEvent } from "svelte-dnd-action";
     import type { RuleSet } from "$lib/schema";
     import type { ReorderInfo } from "$lib/types";
+    import { getToastContext } from "$lib/toast";
     import RuleSetItem from "./RuleSetItem.svelte";
 
     interface Props {
@@ -12,25 +14,44 @@
 
     let { items = $bindable([]), removeFn, updateFn }: Props = $props();
 
+    let showModal = $state(false);
+
+    const createToast = getToastContext();
+
     function handleDndConsider(e: CustomEvent<DndEvent<RuleSet>>) {
         items = e.detail.items;
     }
 
     function handleDndFinish(e: CustomEvent<DndEvent<RuleSet>>) {
-        // create a deep copy of items
-        //
-        // "original" means its `ord` field is unchanged.
-        const originalItems = e.detail.items.map(v => v);
+        updateOrder(e.detail.items);
+    }
 
-        // update order locally
-        items = originalItems.map((item, i) => ({ ...item, ord: i + 1 }));
+    // FIXME: sometimes create invalid orders
+    async function updateOrder(items: RuleSet[]) {
+        // prevent furthur user actions using a modal
+        showModal = true;
 
-        // create update information using original items
-        const updates: ReorderInfo[] = originalItems
-            .filter((item, i) => item.ord !== i + 1)
-            .map((item, i) => ({ id: item.id, newOrder: i + 1 }));
+        try {
+            // create a deep copy of original items
+            //
+            // "original" means its `ord` field is unchanged.
+            const originalItems = items.map(v => v);
 
-        updateFn(updates);
+            // update order locally
+            items = originalItems.map((item, i) => ({ ...item, ord: i + 1 }));
+
+            // create update information using original items
+            const updates: ReorderInfo[] = originalItems
+                .filter((item, i) => item.ord !== i + 1)
+                .map((item, i) => ({ id: item.id, newOrder: i + 1 }));
+
+            await updateFn(updates);
+        } catch (err) {
+            createToast(`error`, `排序失败： ${err}`);
+        } finally {
+            // IMPORTANT: don't forget to close modal
+            showModal = false;
+        }
     }
 </script>
 
@@ -45,3 +66,7 @@
         <RuleSetItem ruleset={ruleset} {removeFn} />
     {/each}
 </div>
+
+<Modal bind:open={showModal} size="xs" dismissable={false}>
+    排序中
+</Modal>
