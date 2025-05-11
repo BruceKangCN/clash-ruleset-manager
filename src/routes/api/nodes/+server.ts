275b50b9340c95cbb78b3b263fd49d884236b38c
Bruce Kang <brucekangcn@gmail.com>
2025-05-11 18:46:11 +0800

    docs: fix API doc annotation issue

--- src/routes/api/nodes/+server.ts
+++ src/routes/api/nodes/+server.ts
@@ -5,9 +5,7 @@ import { getNodes } from "$lib/server/nodes";
  * @api {get} /api/nodes get node groups
  * @apiName GetNodeGroups
  * @apiGroup Node
- * @apiDescription get all node groups
- *
- * @apiSuccess {ClashDashboard.NodeGroup[]} node groups
+ * @apiDescription get all node groups. return node groups as a JSON array.
  */
 export async function GET() {
     const nodeGroups = await getNodes();
